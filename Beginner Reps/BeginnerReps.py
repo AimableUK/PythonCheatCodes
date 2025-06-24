87384390b99383cd2a4466d389a90a4455961d10
UKOBIZABA Aimable <154277022+AimableTrague@users.noreply.github.com>
2025-06-24 07:46:16 +0200

    Added Break, Continue, Pass Statements

--- Beginner Reps/BeginnerReps.py
+++ Beginner Reps/BeginnerReps.py
@@ -122,3 +122,33 @@ for i in range(20,10,-1):
 for i in range(1,21): # 1: Starting Point 21: Ending Point
     if i%5!=0:
         print(i)
+
+# Break, Continue, Pass Statements
+
+# Break Statement
+av = 10
+x = int(input("How many Candies you want"))
+i = 1
+
+while i <= x:
+    if i>av:
+        print("Out of Stock")
+        break
+    print("Candy")
+    i += 1
+print("Bye")
+
+# Continue statement
+for i in range(1,101):
+    if i%3==0 and i%5==0:
+        continue
+    print(i)
+print("Bye")
+
+# PASS: Means noe Code Ignore it
+for i in range(1,101):
+    if i%2!=0:
+        pass
+    else:
+        print(i)
+print("Bye")
