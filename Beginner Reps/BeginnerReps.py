1ba1de0b82786dd18e6140883be4384a8fd6281e
UKOBIZABA Aimable <154277022+AimableTrague@users.noreply.github.com>
2025-07-02 08:36:29 +0200

    1. Operator Overloading 2. Method Overloading 3. Method Overriding

--- Beginner Reps/BeginnerReps.py
+++ Beginner Reps/BeginnerReps.py
@@ -709,3 +709,85 @@ ide = Sublime()
 
 lap1 = Laptop()
 lap1.code(ide)
+
+# Operator Overloading
+class Student:
+
+    def __init__(self, m1, m2):
+        self.m1 = m1
+        self.m2 = m2
+
+    def __add__(self, other):
+        m1 = self.m1 + other.m1
+        m2 = self.m2 + other.m2
+        s3 = Student(m1, m2)
+
+        return s3
+
+    def __gt__(self, other):
+        r1 = self.m1 + self.m2
+        r2 = other.m1 + other.m2
+        if r1 > r2:
+            return True
+        else:
+            return False
+
+    def __str__(self):
+        return '{} {}'.format(self.m1, self.m2)
+
+
+s1 = Student(58, 69)
+s2 = Student(60, 65)
+
+s3 = s1 + s2
+
+if s1 > s2:
+    print("s1 wins")
+else:
+    print("s2 wins")
+
+a = 9
+print(a)
+print(s1)
+
+# Method Overloading
+class Student:
+
+    def __init__(self, m1, m2):
+        self.m1 = m1
+        self.m2 = m2
+
+    def sum(self, a=None, b=None, c=None):
+
+        s = 0
+
+        if a is not None and b is not None and c is not None:
+            s = a + b + c
+        elif a is not None and b is not None:
+            s = a+b
+        else:
+            s = a
+
+        return s
+
+
+s1 = Student(58, 69)
+print(s1.sum(5, 8))
+
+# Method Overriding
+
+
+class A:
+
+    def show(self):
+        print("in A Show")
+
+class B(A):
+
+    def show(self):
+        print("in B show")
+
+
+
+a1 = B()
+a1.show()
