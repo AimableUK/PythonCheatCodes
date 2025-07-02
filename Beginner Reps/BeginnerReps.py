c59e5803bcd2e9faaac3eeb5ac8df817942a99b8
UKOBIZABA Aimable <154277022+AimableTrague@users.noreply.github.com>
2025-07-02 09:48:10 +0200

    1. Iterator 2. Generator

--- Beginner Reps/BeginnerReps.py
+++ Beginner Reps/BeginnerReps.py
@@ -831,3 +831,41 @@ white = WhiteBoard()
 pro = Programmer()
 pro.work(com1,white)
 
+# Iterator:
+class TopTen:
+
+    def __init__(self):
+        self.num = 1
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.num <= 10:
+            val = self.num
+            self.num += 1
+            return val
+        else:
+            raise StopIteration
+
+
+values = TopTen()
+
+for i in values:
+    print(i)
+# Generator:
+def topten():
+    n = 1
+
+    while n <= 10:
+        sq = n * n
+
+        yield sq
+        n += 1
+
+
+values = topten()
+
+for i in values:
+    print(i)
+
